c6a4df1e87abf255dfaed0dcf3c134619514d2bd
saeedhei <saeedhei@gmail.com>
2025-10-05 10:58:08 +0200

    global type added

--- docs/git/pr.md
+++ docs/git/pr.md
@@ -1,3 +1,3 @@
 git add .
-git commit -m "menu updated"
+git commit -m "global type added"
 git push origin main 
---
+++ types/global.d.ts
@@ -0,0 +1,21 @@
+// types\global.d.ts
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
